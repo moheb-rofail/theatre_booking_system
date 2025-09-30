e2085729704541b9f143d5aee823d673d486ad18
Ahmedrashwan <ahmed.rashwan.dev@gmail.com>
2025-10-01 02:36:47 +0300

    Initial commit of theatre booking system

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Booking;
 use Illuminate\Http\Request;
 use App\Http\Requests\BookingRequest;
+use App\Models\Movie;
 
 class BookingController extends Controller
 {
@@ -46,5 +47,48 @@ class BookingController extends Controller
         $booking->save();
         //Booking::find($id)->update($booking);
         return "updating was successful";
+    }
+
+
+
+    // store movies data  by admin
+    public function storeMovie(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'poster' => 'nullable|image',
+            'TypeOfFilm' => 'required|string',
+            'duration' => 'required|string|min:1',
+        ]);
+
+        if ($request->hasFile('poster')) {
+            $posterPath = $request->file('poster')->store('posters', 'public');
+        } else {
+            return response()->json(['message' => 'Poster is required'], 422);
+        }
+
+        // Create movie
+        $movie = Movie::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'poster' => $posterPath,
+            'TypeOfFilm' => $request->TypeOfFilm,
+            'duration' => $request->duration,
+        ]);
+
+        // Response
+        return response()->json([
+            'message' => 'Movie created successfully',
+            'movie' => $movie
+        ], 201);
+    }
+
+
+    // get all movies data to show in user side (home page for user)
+    public function getMovies()
+    {
+        $movies = Movie::orderBy('id', 'DESC')->get();
+        return response()->json($movies);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,18 @@
 
 use App\Http\Controllers\Api\BookingController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Movie;
 
 Route::get('/bookings', [BookingController::class, 'index']);
 Route::get('/bookings/{id}', [BookingController::class, 'show']);
 Route::post('/bookings', [BookingController::class, 'store']);
 Route::put('/bookings/{id}', [BookingController::class, 'update']);
 Route::delete('/bookings/{id}', [BookingController::class, 'destroy']);
+
+
+
+/// route special for admin to store movies data
+Route::post('/movies', [BookingController::class, 'storeMovie']);
+
+/// route to get all movies data to show in user side (home page for user)
+Route::get('/movies', [BookingController::class, 'getMovies']);
