cfccee620bfd15d67352d6dd3760498128bd5b8c
moheb-rofail <dev.moheb.rofail@gmail.com>
2025-10-05 12:20:09 +0300

    fixing errors

--- resources/views/welcome.blade.php
+++
@@ -1,1 +0,0 @@
-<h1>hello Ahmed and moheb</h1>
--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\BookingController;
-use App\Http\Controllers\Api\MovieController;
-use App\Http\Controllers\Api\ValueController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/bookings', [BookingController::class, 'index']);
-Route::get('/bookings/{id}', [BookingController::class, 'show']);
-Route::post('/bookings', [BookingController::class, 'store']);
-Route::put('/bookings/{id}', [BookingController::class, 'update']);
-Route::delete('/bookings/{id}', [BookingController::class, 'destroy']);
-Route::get('/booked_seats/{party_date}/{party_number}', [BookingController::class, 'booked_seats']);
-
-Route::get('/movies', [MovieController::class, 'index']);
-Route::get('/movies/{id}', [MovieController::class, 'show']);
-Route::post('/movies', [MovieController::class, 'store']);
-Route::put('/movies/{id}', [MovieController::class, 'update']);
-Route::delete('/movies/{id}', [MovieController::class, 'destroy']);
-
-// Admin
-Route::post('/settings', [ValueController::class, 'update']);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
